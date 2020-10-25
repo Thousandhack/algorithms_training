a67bc90098835170a3c92f339bb0838eb86ec8f3
Thousandhack <1391053697@qq.com>
2020-10-25 22:47:46 +0800

    用两个栈实现队列

---
+++ 05_TwoStackChangeToQueue.py
@@ -0,0 +1,33 @@
+"""
+用两个栈实现队列
+**题目：**用两个栈来实现一个队列，完成队列的Push和Pop操作。 队列中的元素为int类型。
+
+**题解：**申请两个栈Stack1和Stack2，Stack1当作输入，Stack2当作pop。当Stack2空的时候，将Stack1进行反转，并且输入到Stack2。
+
+首先需要知道的是:  栈是先进后出,队列是先进先出的概念
+
+"""
+
+
+class Solution:
+    def __init__(self):
+        self.Stack1 = []
+        self.Stack2 = []
+
+    def push(self, node):
+        self.Stack1.append(node)
+
+    def pop(self):
+        if self.Stack2 == []:
+            while self.Stack1:
+                self.Stack2.append(self.Stack1.pop())
+            return self.Stack2.pop()
+        return self.Stack2.pop()
+
+
+if __name__ == '__main__':
+    solution = Solution()
+    solution.push(1)
+    solution.push(2)
+    solution.push(3)
+    print(solution.pop())
