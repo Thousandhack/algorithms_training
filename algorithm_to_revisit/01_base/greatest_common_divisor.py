c4323ea7f76e47bc889bde4d4deaa3b96065be6b
Thousandhack <1391053697@qq.com>
2021-05-18 09:00:34 +0800

    最大公约数

---
+++ algorithm_to_revisit/01_base/greatest_common_divisor.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# __author__ hsz
+
+"""
+计算两个非负整数的最大公约数
+"""
+
+
+def greatest(p, q):
+    if q == 0:
+        return p
+    r = p % q
+    return greatest(q, r)
+
+
+if __name__ == "__main__":
+    print(greatest(3, 6))
